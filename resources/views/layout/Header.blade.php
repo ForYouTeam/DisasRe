ed0f34154371f110dbc48175176ab964a419904d
nzrkodi <nzrkodi@gmail.com>
2023-09-07 08:21:45 +0800

    update title

--- resources/views/layout/Header.blade.php
+++ resources/views/layout/Header.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<title>Dashboard V.3 | Kiaalap - Kiaalap Admin Template</title>
+<title>Sistem Pelaporan | @yield('title')</title>
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/favicon.ico')}}">
